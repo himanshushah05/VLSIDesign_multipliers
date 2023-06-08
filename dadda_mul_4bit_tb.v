34f5e1adb8af7fff9d721c5842a7f442f393887c
himanshushah05 <78071859+himanshushah05@users.noreply.github.com>
2023-06-08 19:11:22 +0530

    Update dadda_mul_4bit_tb.v

--- dadda_mul_4bit_tb.v
+++ dadda_mul_4bit_tb.v
@@ -2,7 +2,7 @@ module dadda_mul_4bit_tb();
   reg[3:0] a,b;
   wire[7:0] p;
   reg[7:0] check;
-  dadda_mul_4bit_20bei0013 uut(a,b,p);
+  dadda_mul_4bit uut(a,b,p);
   initial repeat(10) begin
     a = $random;
     b = $random;
