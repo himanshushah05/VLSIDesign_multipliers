1c638cfee6935c7c5c4895e483eb089d339a9fba
himanshushah05 <78071859+himanshushah05@users.noreply.github.com>
2023-06-08 19:10:46 +0530

    Update square_4bit_tb.v

--- square_4bit_tb.v
+++ square_4bit_tb.v
@@ -2,7 +2,7 @@ module square_4bit_tb();
   reg[3:0] a;
   wire[7:0] p;
   reg[7:0] check;
-  square_4bit_20bei0013 uut(a,p);
+  square_4bit uut(a,p);
   initial repeat(10) begin
     a = $random;
     check = a**2;
