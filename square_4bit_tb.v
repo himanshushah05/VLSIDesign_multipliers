b19bd721670c5fffe305d4399e5ddeeabef05ca3
himanshushah05 <78071859+himanshushah05@users.noreply.github.com>
2023-06-08 17:08:04 +0530

    Create square_4bit_tb.v
    4 bit binary square circuit testbench

---
+++ square_4bit_tb.v
@@ -0,0 +1,12 @@
+module square_4bit_tb();
+  reg[3:0] a;
+  wire[7:0] p;
+  reg[7:0] check;
+  square_4bit_20bei0013 uut(a,p);
+  initial repeat(10) begin
+    a = $random;
+    check = a**2;
+    #10
+    $display($time,"%d^2 = %d(%d)",a,p,check);
+  end
+endmodule
