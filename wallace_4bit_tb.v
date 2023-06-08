91856b686f092bd2082ae82a3baed864710c23bc
himanshushah05 <78071859+himanshushah05@users.noreply.github.com>
2023-06-08 16:49:28 +0530

    Create wallace_4bit_tb.v
    4-Bit Wallace-Tree Multiplier Design Testbench

---
+++ wallace_4bit_tb.v
@@ -0,0 +1,13 @@
+module wallace_4bit_tb();
+  reg[3:0] a,b;
+  wire[7:0] p;
+  reg[7:0] check;
+  wallace_4bit uut(a,b,p);
+  initial repeat(10) begin
+    a = $random;
+    b = $random;
+    check = a*b;
+    #10
+    $display($time,"%d*%d = %d(%d)",a,b,p,check);
+  end
+endmodule
